d890114615fa1e4cf8b3e521f924f773b926e25d
jrmeurer <jrmeurer@wisc.edu>
2023-06-01 16:01:03 -0600

    Fix fallback provider network

--- src/utils/fallback-provider.ts
+++ src/utils/fallback-provider.ts
@@ -1,4 +1,4 @@
-import { FallbackProvider, WebSocketProvider } from 'ethers';
+import { FallbackProvider, Networkish, WebSocketProvider } from 'ethers';
 import { PollingJsonRpcProvider } from './polling-json-rpc-provider';
 
 export type FallbackProviderJsonConfig = {
@@ -15,20 +15,19 @@ export type ProviderJson = {
 export const createFallbackProviderFromJsonConfig = (
   config: FallbackProviderJsonConfig,
 ): FallbackProvider => {
+  const network: Networkish = Number(config.chainId);
   try {
     const providers = config.providers.map(json => {
       const isWebsocket = json.provider.startsWith('wss');
       const provider = isWebsocket
-        ? new WebSocketProvider(json.provider, Number(config.chainId))
-        : new PollingJsonRpcProvider(json.provider, Number(config.chainId));
+        ? new WebSocketProvider(json.provider, network)
+        : new PollingJsonRpcProvider(json.provider, network);
       return {
         ...json,
         provider,
       };
     });
-
-    const quorum = 1;
-    return new FallbackProvider(providers, quorum);
+    return new FallbackProvider(providers, network);
   } catch (err) {
     if (!(err instanceof Error)) {
       throw err;
