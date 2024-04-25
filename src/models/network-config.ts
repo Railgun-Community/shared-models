6e76bb5b6273fac22406174c4453afa02795fc06
jacobmakarsky <jacobmakarsky@gmail.com>
2024-04-25 19:27:17 -0400

    added polygon amoy testnet

--- src/models/network-config.ts
+++ src/models/network-config.ts
@@ -12,6 +12,7 @@ export enum NetworkName {
 
   // Testnets
   EthereumSepolia = 'Ethereum_Sepolia',
+  PolygonAmoy = 'Polygon_Amoy',
 
   // Dev only
   Hardhat = 'Hardhat',
@@ -74,6 +75,7 @@ export const RailgunProxyContract: Record<NetworkName, string> = {
 
   // Test nets
   [NetworkName.EthereumSepolia]: '0x942D5026b421cf2705363A525897576cFAdA5964',
+  [NetworkName.PolygonAmoy]: '0x19b620929f97b7b990801496c3b361ca5def8c71',
 
   // Dev only
   [NetworkName.Hardhat]: '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
@@ -94,6 +96,7 @@ export const RelayAdaptContract: Record<NetworkName, string> = {
 
   // Test nets
   [NetworkName.EthereumSepolia]: '0xCc1C4D2B362c3a0Fb19f734A896A58C81A062dc8',
+  [NetworkName.PolygonAmoy]: '0xc7FfA542736321A3dd69246d73987566a5486968',
 
   // Dev only
   [NetworkName.Hardhat]: '0x0355B7B8cb128fA5692729Ab3AAa199C1753f726',
@@ -114,6 +117,7 @@ export const RailgunProxyDeploymentBlock: Record<NetworkName, number> = {
 
   // Test nets
   [NetworkName.EthereumSepolia]: 4495479,
+  [NetworkName.PolygonAmoy]: 28083766,
 
   // Dev only
   [NetworkName.Hardhat]: 0,
@@ -134,6 +138,7 @@ export const BaseTokenWrappedAddress: Record<NetworkName, string> = {
 
   // Test nets
   [NetworkName.EthereumSepolia]: '0x97a36608DA67AF0A79e50cb6343f86F340B3b49e', // (Sepolia) WETH
+  [NetworkName.PolygonAmoy]: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // (Amoy) WMATIC
 
   // Dev only
   [NetworkName.Hardhat]: '0x8198f5d8F8CfFE8f9C413d98a0A55aEB8ab9FbB7', // (Hardhat) WETH
@@ -161,6 +166,7 @@ export const RailgunPoseidonMerkleAccumulatorV3Contract: Record<
 
   // Test nets
   [NetworkName.EthereumSepolia]: '', // TODO
+  [NetworkName.PolygonAmoy]: '', // TODO
 
   // Dev only
   [NetworkName.Hardhat]: '0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5',
@@ -184,6 +190,7 @@ export const RailgunPoseidonMerkleVerifierV3Contract: Record<
 
   // Test nets
   [NetworkName.EthereumSepolia]: '', // TODO
+  [NetworkName.PolygonAmoy]: '', // TODO
 
   // Dev only
   [NetworkName.Hardhat]: '0xfbC22278A96299D91d41C453234d97b4F5Eb9B2d',
@@ -204,6 +211,7 @@ export const RailgunTokenVaultV3Contract: Record<NetworkName, string> = {
 
   // Test nets
   [NetworkName.EthereumSepolia]: '', // TODO
+  [NetworkName.PolygonAmoy]: '', // TODO
 
   // Dev only
   [NetworkName.Hardhat]: '0xD84379CEae14AA33C123Af12424A37803F885889',
@@ -227,6 +235,7 @@ export const RailgunPoseidonMerkleAccumulatorV3DeploymentBlock: Record<
 
   // Test nets
   [NetworkName.EthereumSepolia]: 0, // TODO
+  [NetworkName.PolygonAmoy]: 0, // TODO
 
   // Dev only
   [NetworkName.Hardhat]: 0,
@@ -501,6 +510,44 @@ export const NETWORK_CONFIG: Record<NetworkName, Network> = {
         NetworkName.EthereumSepolia
       ],
     supportsV3: false,
+  },
+  [NetworkName.PolygonAmoy]: {
+    deprecated: false,
+    chain: {
+      type: ChainType.EVM,
+      id: 80002,
+    },
+    name: NetworkName.PolygonAmoy,
+    publicName: 'Amoy Testnet',
+    shortPublicName: 'Amoy',
+    coingeckoId: 'polygon-pos',
+    baseToken: {
+      symbol: 'MATIC',
+      wrappedSymbol: 'WMATIC',
+      wrappedAddress: BaseTokenWrappedAddress[NetworkName.PolygonAmoy],
+      decimals: 18,
+    },
+    proxyContract: RailgunProxyContract[NetworkName.PolygonAmoy],
+    relayAdaptContract: RelayAdaptContract[NetworkName.PolygonAmoy],
+    relayAdaptHistory: [
+      '0x8B936B018CeA1c1cc14961CAdabD36E7fe9192cD', // Initial deployment
+      '0x80efF1a8939C9B6E8C61Ed5Be683Eda21088B7E6', // V3.0 Update - Nov 2022
+      '0x17D36875D723Cf0dA250d404Ef4cA0aABE105837', // V3.1 Update - Jan 2023
+    ],
+    deploymentBlock: RailgunProxyDeploymentBlock[NetworkName.PolygonAmoy],
+    isDevOnlyNetwork: true,
+    isTestnet: true,
+    defaultEVMGasType: EVMGasType.Type2,
+    poseidonMerkleAccumulatorV3Contract:
+      RailgunPoseidonMerkleAccumulatorV3Contract[NetworkName.PolygonAmoy],
+    poseidonMerkleVerifierV3Contract:
+      RailgunPoseidonMerkleVerifierV3Contract[NetworkName.PolygonAmoy],
+    tokenVaultV3Contract: RailgunTokenVaultV3Contract[NetworkName.PolygonAmoy],
+    deploymentBlockPoseidonMerkleAccumulatorV3:
+      RailgunPoseidonMerkleAccumulatorV3DeploymentBlock[
+        NetworkName.PolygonAmoy
+      ],
+    supportsV3: true,
   },
   [NetworkName.PolygonMumbai_DEPRECATED]: {
     deprecated: true,
