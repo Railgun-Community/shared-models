04faafc0368c8c44479a36bbbf8c7c4a0534a6fe
bhflm <germanbonin@gmail.com>
2025-01-13 14:07:07 -0300

    chore: add constants.ts to segregate errors acording to categories, match them 1:1 to existing errros

---
+++ src/utils/errors/constants.ts
@@ -0,0 +1,88 @@
+import { isDefined } from '../util';
+
+type ErrorDefinition = {
+  matches: string[];
+  message: string;
+};
+
+type CustomErrorMapping = {
+  [key: string]: ErrorDefinition;
+};
+
+const CUSTOM_ERRORS: CustomErrorMapping = {
+  CONNECTION_ERROR: {
+    matches: ['quorum', 'could not connect to'],
+    message: 'Could not connect.'
+  },
+  RPC_ERROR: {
+    matches: ['call revert exception', 'missing revert data', 'error while dialing dial tcp'],
+    message: 'Failed to connect to RPC.'
+  },
+  KNOWN_TRANSACTION: {
+    matches: ['already known'],
+    message: 'Transaction successful but ethers request for TXID failed.'
+  },
+  LOW_REPLACEMENT_FEE: {
+    matches: ['replacement fee too low'],
+    message: 'Nonce is used in a pending transaction, and replacement fee is too low. Please increase your network fee to replace the pending transaction.'
+  },
+  LOW_GAS: {
+    matches: ['intrinsic gas too low'],
+    message: 'Gas price rejected. Please select a higher gas price or resubmit.'
+  },
+  LOW_GAS_RESUBMIT: {
+    matches: ['intrinsic gas too low'],
+    message: 'Gas price rejected. Please select a higher gas price and resubmit.'
+  }, // @@TODO: Find a better suited message for this
+  INSUFFICIENT_GAS: {
+    matches: ['insufficient funds for intrinsic'],
+    message: 'Insufficient gas to process transaction.'
+  },
+  NONCE_USED: {
+    matches: ['nonce has already been used'],
+    message: 'Nonce already used: the transaction was already completed.'
+  }, 
+  LOW_PRIVATE_BALANCE: {
+    matches: ['spendable private balance too low', 'broadcaster fee'],
+    message: 'Private balance too low to pay broadcaster fee.'
+  }
+};
+
+const RAILGUN_ERRORS: CustomErrorMapping = {
+  INVALID_NFT_NOTE: {
+    matches: ['invalid nft note value'],
+    message: 'RailgunSmartWallet: Invalid NFT Note Value.'
+  },
+  UNSUPPORTED_TOKEN: {
+    matches: ['unsupported token'],
+    message: 'RailgunSmartWallet: Unsupported Token. This token cannot interact with the RAILGUN contract.'
+  },
+  INVALID_NOTE_VALUE: {
+    matches: ['invalid note value'],
+    message: 'RailgunSmartWallet: Invalid Note Value. Please submit transaction with a corrected amount.'
+  },
+  INVALID_ADAPT_CONTRACT_SENDER: {
+    matches: ['invalid adapt contract as sender'],
+    message: 'RailgunSmartWallet: Invalid Adapt Contract as Sender. Please update your frontend to current Adapt module versions.'
+  },
+  INVALID_MERKLE_ROOT: {
+    matches: ['invalid merkle root'],
+    message: 'RailgunSmartWallet: Invalid Merkle Root. Please sync your balances and try again.'
+  },
+  NOTE_SPENT: {
+    matches: ['note already spent'],
+    message: 'RailgunSmartWallet: Note Already Spent. Please sync your balances and try again.'
+  },
+  INVALID_NOTE_CIPHERTEXT_ARRAY_LENGTH: {
+    matches: ['invalid note ciphertext array length'],
+    message: 'RailgunSmartWallet: Invalid Note Ciphertext Array Length. Please sync balances and re-prove your transaction.'
+  },
+  INVALID_WITHDRAW_NOTE: {
+    matches: ['invalid withdraw note'],
+    message: 'RailgunSmartWallet: Invalid Unshield Note. Please sync balances and re-prove your transaction.'
+  },
+  INVALID_SNARK_PROOF: {
+    matches: ['invalid snark proof'],
+    message: 'RailgunSmartWallet: Invalid Snark Proof. Please re-prove your transaction.'
+  }
+};
