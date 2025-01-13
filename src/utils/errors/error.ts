d4a00ae396e329cf5a18de16e83143f23be796da
bhflm <germanbonin@gmail.com>
2025-01-13 14:07:07 -0300

    chore: add constants.ts to segregate errors acording to categories, match them 1:1 to existing errros

--- src/utils/errors/error.ts
+++ src/utils/errors/error.ts
@@ -31,66 +31,73 @@ const validAscii = (str: string) => {
   );
 };
 
+const isRailgunError = (cause: Error): boolean => cause.message.toLowerCase().includes('railgunsmartwallet')
+
 export const sanitizeError = (cause: Error): Error => {
   if (isDefined(cause) && cause.message) {
     const lowercaseMsg = cause.message.toLowerCase();
+
+    if (isRailgunError(cause)) {
+      return cause;
+    }
+
     if (
-      lowercaseMsg.includes('quorum') ||
-      lowercaseMsg.includes('could not connect to')
+      lowercaseMsg.includes('quorum') || // Connection error
+      lowercaseMsg.includes('could not connect to') // Connection error
     ) {
       return new Error('Could not connect.', { cause });
     }
-    if (lowercaseMsg.includes('call revert exception')) {
-      return new Error('Failed to connect to RPC.', { cause });
+    if (lowercaseMsg.includes('call revert exception')) { 
+      return new Error('Failed to connect to RPC.', { cause }); // RPC_ERROR
     }
     if (lowercaseMsg.includes('already known')) {
       return new Error(
         'Transaction successful but ethers request for TXID failed.',
-        { cause },
+        { cause }, // KNOWN_TRANSACTION
       );
     }
     if (lowercaseMsg.includes('missing revert data')) {
-      return new Error('RPC connection error.', { cause });
+      return new Error('RPC connection error.', { cause }); // RPC_ERROR
     }
     if (
       lowercaseMsg.includes(
         'transaction may fail or may require manual gas limit',
       )
     ) {
-      return new Error('Unknown error. Transaction failed.', { cause });
+      return new Error('Unknown error. Transaction failed.', { cause }); // @@TODO: Replace or categorize better 
     }
     if (lowercaseMsg.includes('replacement fee too low')) {
       return new Error(
         'Nonce is used in a pending transaction, and replacement fee is too low. Please increase your network fee to replace the pending transaction.',
-        { cause },
+        { cause }, // LOW_REPLACEMENT_FEE
       );
     }
     if (lowercaseMsg.includes('intrinsic gas too low')) {
       return new Error(
         'Gas price rejected. Please select a higher gas price or resubmit.',
-        { cause },
+        { cause }, // LOW_GAS
       );
     }
     if (lowercaseMsg.includes('transaction underpriced')) {
       return new Error(
         'Gas fee too low. Please select a higher gas price and resubmit.',
-        { cause },
+        { cause }, // LOW_GAS_RESUBMIT
       );
     }
     if (lowercaseMsg.includes('insufficient funds for intrinsic')) {
-      return new Error('Insufficient gas to process transaction.', { cause });
+      return new Error('Insufficient gas to process transaction.', { cause }); // INSUFFICIENT_GAS
     }
     if (lowercaseMsg.includes('nonce has already been used')) {
       return new Error(
         // Do not change 'Nonce already used' string of Error message.
         'Nonce already used: the transaction was already completed.',
-        { cause },
+        { cause }, // NONCE_USED
       );
     }
     if (lowercaseMsg.includes('error while dialing dial tcp')) {
       return new Error(
         'Error while connecting to RPC provider. Please try again.',
-        { cause },
+        { cause }, // RPC_ERROR
       );
     }
     if (
@@ -98,58 +105,58 @@ export const sanitizeError = (cause: Error): Error => {
       lowercaseMsg.includes('broadcaster fee')
     ) {
       return new Error('Private balance too low to pay broadcaster fee.', {
-        cause,
+        cause, // LOW_PRIVATE_BALANCE
       });
     }
 
     // Custom RAILGUN contract error messages
     if (lowercaseMsg.includes('railgunsmartwallet')) {
-      if (lowercaseMsg.includes('invalid nft note value')) {
-        return new RailgunContractError('RailgunSmartWallet: Invalid NFT Note Value.', cause);
+      if (lowercaseMsg.includes('invalid nft note value')) { // INVALID_NFT_NOTE
+        return new RailgunContractError('RailgunSmartWallet: Invalid NFT Note Value.', cause); // INVALID_NFT_NOTE
       }
       if (lowercaseMsg.includes('unsupported token')) {
         return new RailgunContractError(
-          'RailgunSmartWallet: Unsupported Token. This token cannot interact with the RAILGUN contract.',
+          'RailgunSmartWallet: Unsupported Token. This token cannot interact with the RAILGUN contract.',// UNSUPPORTED_TOKEN
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid note value')) {
+      if (lowercaseMsg.includes('invalid note value')) {  // INVALID_NOTE_VALUE
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Note Value. Please submit transaction with a corrected amount.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid adapt contract as sender')) {
+      if (lowercaseMsg.includes('invalid adapt contract as sender')) { // INVALID_ADAPT_CONTRACT_SENDER
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Adapt Contract as Sender. Please update your frontend to current Adapt module versions.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid merkle root')) {
+      if (lowercaseMsg.includes('invalid merkle root')) { // INVALID_MERKLE_ROOT
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Merkle Root. Please sync your balances and try again.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('note already spent')) {
+      if (lowercaseMsg.includes('note already spent')) { // NOTE_SPENT
         return new RailgunContractError(
           'RailgunSmartWallet: Note Already Spent. Please sync your balances and try again.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid note ciphertext array length')) {
+      if (lowercaseMsg.includes('invalid note ciphertext array length')) { // INVALID_NOTE_CIPHERTEXT_ARRAY_LENGTH
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Note Ciphertext Array Length. Please sync balances and re-prove your transaction.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid withdraw note')) {
+      if (lowercaseMsg.includes('invalid withdraw note')) { // INVALID_WITHDRAW_NOTE
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Unshield Note. Please sync balances and re-prove your transaction.',
           cause,
         );
       }
-      if (lowercaseMsg.includes('invalid snark proof')) {
+      if (lowercaseMsg.includes('invalid snark proof')) { // INVALID_SNARK_PROOF
         return new RailgunContractError(
           'RailgunSmartWallet: Invalid Snark Proof. Please re-prove your transaction.',
           cause,
@@ -165,5 +172,5 @@ export const sanitizeError = (cause: Error): Error => {
     );
   }
 
-  return new Error('Unknown error. Please try again.', { cause });
+  return new CustomError('Unknown error. Please try again', cause);
 };
