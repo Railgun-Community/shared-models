0e4c319bf186ec56676a64d0f1a86ef176712687
bhflm <germanbonin@gmail.com>
2025-01-14 11:34:44 -0300

    chore: early return for undefined message case, check just for message inside cause since its type Error

--- src/utils/__tests__/error.test.ts
+++ src/utils/__tests__/error.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { sanitizeError } from '../errors/error';
--- src/utils/errors/error.ts
+++ src/utils/errors/error.ts
@@ -1,6 +1,5 @@
 import { CustomErrorMapping, ErrorDefinition } from './types';
 import { STRING_PREFIX_AFTER_UNICODE_REPLACEMENT, RAILGUN_ERRORS, CUSTOM_ERRORS, INVALID_ASCII_REGEX } from './constants';
-import { isDefined } from '../util';
 
 const validAscii = (str: string) => {
   return str.replace(
@@ -23,32 +22,32 @@ const findMatchingError = (errorMessage: string, errorMapping: CustomErrorMappin
 const isRailgunError = (cause: Error): boolean => cause.message.toLowerCase().includes('railgunsmartwallet')
 
 export const sanitizeError = (cause: Error): Error => {
-  if (isDefined(cause) && cause.message) {
-    if (isRailgunError(cause)) {
-      const matchedRailgunError = findMatchingError(cause.message, RAILGUN_ERRORS);
-      if (matchedRailgunError) {
-        return new Error(matchedRailgunError.message, cause);
-      }
-      return new Error('Uknown Railgun Smart Wallet Error.', cause);
-    }
-
-    const matchedCustomError = findMatchingError(cause.message, CUSTOM_ERRORS);
-
-    if (matchedCustomError) {
-      return new Error(matchedCustomError.message, cause);
-    }
-
-    // If no error is matched we return the original sanitized error
-    const errorMessage = validAscii(cause.message).replace(
-      `:${STRING_PREFIX_AFTER_UNICODE_REPLACEMENT}`,
-      ': ',
-    );
-
-    return new Error(
-      errorMessage,
-      cause
-    );
+  if (!cause.message) {
+    return new Error('Unknown error. Please try again.', { cause });
   }
 
-  return new Error('Unknown error. Please try again.', cause);
+  if (isRailgunError(cause)) {
+    const matchedRailgunError = findMatchingError(cause.message, RAILGUN_ERRORS);
+    if (matchedRailgunError) {
+      return new Error(matchedRailgunError.message, cause);
+    }
+    return new Error('Uknown Railgun Smart Wallet Error.', cause);
+  }
+
+  const matchedCustomError = findMatchingError(cause.message, CUSTOM_ERRORS);
+
+  if (matchedCustomError) {
+    return new Error(matchedCustomError.message, cause);
+  }
+
+  // If no error is matched we return the original sanitized error
+  const errorMessage = validAscii(cause.message).replace(
+    `:${STRING_PREFIX_AFTER_UNICODE_REPLACEMENT}`,
+    ': ',
+  );
+
+  return new Error(
+    errorMessage,
+    cause
+  );
 };
