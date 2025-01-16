142ef1fe4652d7558581673cc7daac76b422488a
bhflm <germanbonin@gmail.com>
2025-01-16 20:34:26 -0300

    fix: correct error dir test path

--- src/utils/__tests__/error.test.ts
+++ src/utils/__tests__/error.test.ts
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { sanitizeError } from '../errors';
+import { sanitizeError } from '../error';
 
 chai.use(chaiAsPromised);
 const { expect } = chai;
