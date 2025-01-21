d25cb0325c15ccf90d1a5e6c5594bb6a994c8a73
jacobmakarsky <jacobmakarsky@gmail.com>
2025-01-21 14:52:29 -0500

    add support for web socket provider in health check and matching test

--- src/utils/__tests__/available-rpc.test.ts
+++ src/utils/__tests__/available-rpc.test.ts
@@ -8,8 +8,8 @@ import {
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
-describe('available-rpc', () => {
-  it('Should check fallback provider cascade', async () => {
+describe.only('available-rpc', () => {
+  it('Should check fallback provider cascade for FallbackProvider of HTTPS RPCs', async () => {
     const config: FallbackProviderJsonConfig = {
       chainId: 1,
       providers: [
@@ -21,6 +21,24 @@ describe('available-rpc', () => {
         },
         { provider: 'https://cloudflare-eth.com/', priority: 3, weight: 1 },
         { provider: 'https://rpc.ankr.com/eth', priority: 3, weight: 1 },
+      ],
+    };
+
+    const fallbackProvider = createFallbackProviderFromJsonConfig(config);
+
+    await fallbackProvider.getBlockNumber();
+  }).timeout(5000);
+
+  it('Should check fallback provider cascade for FallbackProvider of WSS RPC', async () => {
+    const config: FallbackProviderJsonConfig = {
+      chainId: 1,
+      providers: [
+        {
+          provider: 'wss://ethereum-rpc.publicnode.com',
+          priority: 1,
+          weight: 2,
+          stallTimeout: 2500,
+        }
       ],
     };
 
